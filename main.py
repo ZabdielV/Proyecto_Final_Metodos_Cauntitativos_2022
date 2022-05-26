4a9280966b1d3591368c15dc268f22a9f5580158
A01377942 <a01377942@itesm.mx>
2022-05-26 17:01:07 -0500

    Fichero Update

--- main.py
+++ main.py
@@ -4,74 +4,16 @@ Luis Jonathan Rosas Ramos A01377942
 Zabdiel Valentin Garduño Vivanco A01377950
 """
 
-import random
 import plotly.express as px
 import pandas as pd
+from pathlib import Path
 
+myfile = Path('./pruebas.txt')
+myfile.touch(exist_ok=True)
+f = open('pruebas.txt', 'r+')
 
 ejeX = []
 ejeY = []
-# TCP Method
-
-
-def TCP():
-
-    # tiempo simulado que transcurre
-    tiempo = 0
-
-    # Valor inicial de cwnd
-    initialCwnd = 2
-
-    # ritmo en el que se envia paquetes, conocido como Congestion window
-    cwnd = initialCwnd
-
-    # limite de velocidad, concido como initial-slow-start-threshold
-    sst = 100
-
-    # Variables para simular si hay una perdida o TimeOut
-
-    # connection phase
-    print("Sending SYN message")
-    print("receiving SYN+ACK message")
-    print("starting InitiateTransferPhase")
-
-    # slow start: se incrementa la velocidad de forma exponencial hasta llegar a un limite sst
-    while(cwnd < sst):
-        cwnd += 1
-        tiempo += 0.5
-        print("tiempo (s): ", tiempo)
-        print("increasing cwnd: ", cwnd)
-
-    print("cwnd is greater than sst, starting Transfer phase")
-
-    # Transfer phase - Congestion Avoidance
-
-    # Se incrementa de forma lineal cwnd hasta que se reporte una perdida (loss) o timeout
-    while(tiempo < 100):
-        print("Se recibio un ACK, incrementando cwnd")
-        if(cwnd >= sst):
-            cwnd += 1 / cwnd
-        else:
-            cwnd += 1
-
-        # simular que hay una perdida
-        if(tiempo == 60):
-            print("Loss detected ***************")
-            cwnd = cwnd/2
-            sst = cwnd
-
-        # simular que hay un timeout
-        # simular un TimeOut
-        if(tiempo == 80):
-            print("timeout ************")
-            sst = cwnd/2
-            cwnd = initialCwnd
-
-        print("cwnd: ", cwnd)
-        print("sst: ", sst)
-        print("tiempo (s): ", tiempo)
-        tiempo += 0.5
-
 
 def scalableTCP():
 
@@ -94,26 +36,27 @@ def scalableTCP():
     bS = 0.875
 
     # connection phase
-    print("Sending SYN message")
-    print("receiving SYN+ACK message")
-    print("starting InitiateTransferPhase")
+    f.write("Sending SYN message\n")
+    f.write("Receiving SYN+ACK message\n")
+    f.write("Starting InitiateTransferPhase\n")
+    f.write("--------------------------------------\n")
 
     # slow start: se incrementa la velocidad de forma exponencial hasta llegar a un limite lws
     while(cwnd < lws):
         cwnd += 1
         tiempo += 0.5
-        print("tiempo (s): ", tiempo)
-        print("increasing cwnd: ", cwnd)
+        f.write("tiempo (s): " + str(tiempo) + "\n")
+        f.write("increasing cwnd: " + str(cwnd) + "\n")
         ejeX.append(tiempo)
         ejeY.append(cwnd)
-    print("cwnd is greater than lws,starting ScalableTCPcongestionAvoidance")
+    f.write("cwnd is greater than lws,starting ScalableTCPcongestionAvoidance\n")
 
     # Transfer phase - ScalableTCPcongestionAvoidance
 
     # Se cwnd hasta que se reporte una perdida (loss) o timeout
     while(tiempo < 100):
 
-        print("Se recibio un ACK, incrementando cwnd")
+        f.write("Se recibio un ACK, incrementando cwnd\n")
         if(cwnd >= lws):
             cwnd += aS
         else:
@@ -121,27 +64,28 @@ def scalableTCP():
 
         # simular que hay una perdida
         if(tiempo == 60):
-            print("Loss detected ***************")
+            f.write("Loss detected ***************\n")
             cwnd = cwnd*bS
             sst = cwnd
 
         # simular que hay un timeout
         # simular un TimeOut
         if(tiempo == 80):
-            print("timeout ************")
+            f.write("Timeout ************\n")
             cwnd = cwnd * bS
             sst = cwnd
             cwnd = initialCwnd
 
-        print("cwnd: ", cwnd)
-        print("sst: ", sst)
-        print("tiempo (s): ", tiempo)
+        f.write("cwnd: " + str(cwnd) + "\n")
+        f.write("sst: " + str(sst) + "\n")
+        f.write("tiempo (s): " + str(tiempo) + "\n")
         tiempo += 0.5
         ejeX.append(tiempo)
         ejeY.append(cwnd)
 
 
 def main():
+
     # TCP()
     scalableTCP()
     df = pd.DataFrame(dict(
@@ -153,6 +97,8 @@ def main():
                   title="Scalable TCP Congestion Avoidance (simulación)")
 
     fig.show()
+    f.write('-------------------------------------\n')
+    f.close()
 
 
 main()
